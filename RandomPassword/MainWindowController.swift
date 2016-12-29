d1e694a111f16de5a6e6bb4b8a4db0e6eade25f3
jbaker10 <jer.baker10@gmail.com>
2016-12-29 12:53:26 -0500

    Adding ability to specify length. Adding failure errors

--- RandomPassword/MainWindowController.swift
+++ RandomPassword/MainWindowController.swift
@@ -22,16 +22,29 @@ class MainWindowController: NSWindowController {
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
     
-    @IBOutlet weak var getPasswordLength: NSPopUpButton!
+    @IBOutlet weak var getPasswordLength: NSTextField!
     
     @IBAction func generatePassword(sender: AnyObject) {
         // Get a random string of length 8
-        let passwordLength = getPasswordLength.titleOfSelectedItem
-        
-        let length = Int(passwordLength!)
-        let password = generateRandomString(length: length!)
-        
-        //Tell the text field display the string
-        textField.stringValue = password
+        let passwordLength = getPasswordLength.stringValue
+        if passwordLength == "" {
+            print("No input")
+            textField.stringValue = "Enter a value between 8 and 22"
         }
+        else if Int(passwordLength)! < 8 {
+            print("Too short of a number :(")
+            textField.stringValue = "Enter a value between 8 and 22"
+        }
+        else if Int(passwordLength)! > 22 {
+            print("Too long of a number :(")
+            textField.stringValue = "Enter a value between 8 and 22"
+        }
+        else {
+            let length = Int(passwordLength)
+            let password = generateRandomString(length: length!)
+            
+            //Tell the text field display the string
+            textField.stringValue = password
+        }
+    }
 }
